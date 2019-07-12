e0bc9fbe87179d314dc95398c13063fac73e0b24
registered <registered>
2019-07-12 16:30:06 +0530

    quit code added

--- src/oddNo.java
+++ src/oddNo.java
@@ -1,4 +1,4 @@
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,10 +8,11 @@ public class oddNo {
 	public  void main(String args[]) 
 	{
 		
-			System.setProperty("webdriver.chrome.driver","C:\\Users\\Lakshmi\\git\\julybatch\\src\\Login.java" );
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\Lakshmi\\git\\julybatch\\driver\\chromedriver.exe" );
 			WebDriver driver = new ChromeDriver();
 			driver.get("https://www.flipkart.com/");
 			
+			driver.quit();
 		}
 	
 }
