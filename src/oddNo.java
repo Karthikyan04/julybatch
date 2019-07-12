e0bb8b0aa1a75ef491b9e974bf32b15c3db45e13
registered <registered>
2019-07-12 12:15:26 +0530

    pull all the data

--- src/oddNo.java
+++ src/oddNo.java
@@ -5,13 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class oddNo {
 
-	public  void main(String args[]) 
-	{
-		
-			System.setProperty("webdriver.chrome.driver","C:\\Users\\Lakshmi\\git\\julybatch\\src\\Login.java" );
+	public static void main(String[] args) {
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\Raji\\git\\julybatch\\julybatch\\julybatch\\driver\\chromedriver.exe");
 			WebDriver driver = new ChromeDriver();
 			driver.get("https://www.flipkart.com/");
-			
+		WebElement r=driver.findElement(By.xpath("(//input[@type='text'])[2]"));
+		r.sendKeys("rajechandran777@gmail.com");
+		WebElement r1=driver.findElement(By.xpath("//input[@type='password']"));
+		r1.sendKeys("Raje@143");
+		WebElement r2=driver.findElement(By.xpath("(//button[@type='submit'])[2]"));
+		r2.click();
 		}
 	
 }
