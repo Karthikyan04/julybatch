6af57e33351cd7e9c6a0ff9100957d15caae2a95
registered <registered>
2019-07-12 17:44:31 +0530

    adding to the master

--- src/oddNo.java
+++ src/oddNo.java
@@ -17,24 +17,6 @@ public class oddNo {
 			System.setProperty("webdriver.chrome.driver","C:\\Users\\Raji\\git\\julybatch\\julybatch\\julybatch\\driver\\chromedriver.exe");
 			WebDriver driver = new ChromeDriver();
 			driver.get("https://www.flipkart.com/");
-		WebElement ra=driver.findElement(By.xpath("(//input[@type='text'])[2]"));
-		ra.sendKeys("rajechandran777@gmail.com");
-		WebElement r1=driver.findElement(By.xpath("//input[@type='password']"));
-		r1.sendKeys("Raje@143");
-		WebElement r2=driver.findElement(By.xpath("(//button[@type='submit'])[2]"));
-		r2.click();
-			driver.manage().window().maximize();
-		    driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Mobiles");
-		    Robot r = new Robot();
-		    r.keyPress(KeyEvent.VK_ENTER);
-		    r.keyRelease(KeyEvent.VK_ENTER);
-			Thread.sleep(2000);
-			List<WebElement> mob = new ArrayList<>();
-					mob = driver.findElements(By.className("_3wU53n"));
-					for (WebElement x : mob) {
-						System.out.println("Mobile Names");
-						
-						String text = x.getText();	
-						System.out.println(text);
+			driver.quit();
+		}
 					}
-					}}
