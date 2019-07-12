d4a076ad00c9770f444c8dc959bf4e151caa945a
ThulasiRithu <tulsilakshmi@gmail.com>
2019-07-12 20:11:13 +0530

    Code for selecting mobile from flipkart

--- src/oddNo.java
+++ src/oddNo.java
@@ -10,13 +10,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class oddNo {
-
-
-	public static void main(String[] args) throws InterruptedException, AWTException 
-	{
-			System.setProperty("webdriver.chrome.driver","C:\\Users\\Raji\\git\\julybatch\\julybatch\\julybatch\\driver\\chromedriver.exe");
+	public static void main(String[] args) throws AWTException	{
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\ELCOT\\eclipse-workspace\\julybatch\\driver\\chromedriver.exe");
 			WebDriver driver = new ChromeDriver();
-			driver.get("https://www.flipkart.com/");
-			driver.quit();
-		}
-					}
+			driver.get("https://www.flipkart.com/search?q=Mobiles&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=off&as=off");
+		   Robot r1 = new Robot();
+		  driver.findElement(By.xpath("//div[text()='Redmi Note 7S (Ruby Red, 64 GB)']")).click();
+		    r1.keyPress(KeyEvent.VK_ENTER);
+		    r1.keyRelease(KeyEvent.VK_ENTER);
+	}
+}
